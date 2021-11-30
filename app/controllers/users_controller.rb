535e30fba62db72e5fc3ddfd290e02dee0d0d2a3
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-30 18:19:12 +0300

    Generate a controller for users to sign in through

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authenticate_user, except: [:create]
 
   # GET /users
   def index
