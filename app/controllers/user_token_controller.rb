68fd55a4de25a6dd6634e4df8152a22a87676aa4
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-02 13:47:50 +0300

    Access specific user using their valid token

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,7 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
+  def create
+    cookie[:token] = access_token.token
+    render json: { username: User.find(access_token.payload[:user]).username }, status: :created
+  end
 end
