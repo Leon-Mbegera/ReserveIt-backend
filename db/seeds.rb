1b27f94ad9ad2dc9f17b2532161576e6d3165e5c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-17 15:54:12 +0300

    Create instances of car model in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Car.create([
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDISPF55HkKkYnLFalChMNRLdI-nCktbdTEg&usqp=CAU",
+    model: "Subaru",
+    mileage: "57398 km",
+    price: 945000
+  }
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfwhF_pUx5qGNWWu_w_q9wl9wjcDfhISfRZg&usqp=CAU",
+    model: "Toyota Harrier",
+    mileage: "57398 km",
+    price: 3315000
+  }
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQk1JcxRZRcDUSRLJ-JioZUMTObZkLXZY55rg&usqp=CAU",
+    model: "Mark X",
+    mileage: "57398 km",
+    price: 3315000
+  }
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs5lSO6XSwgbB2NBBLCQSXSCVKXy203oHGXg&usqp=CAU",
+    model: "Range Rover",
+    mileage: "57398 km",
+    price: 3315000
+  }
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYKcPd2xGYEQq_a5tt873B6k7YWqXXq4c03g&usqp=CAU",
+    model: "BMW 5 Series",
+    mileage: "57398 km",
+    price: 3315000
+  }
+  {
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSllxfMUKW-oiVUUkx7uqCfXNQeZ4nEUInXbA&usqp=CAU",
+    model: "Pickup truck",
+    mileage: "57398 km",
+    price: 3315000
+  }
+])
