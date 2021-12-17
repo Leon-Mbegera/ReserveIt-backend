04b1fe0aa2b3252867ecdac8ca932a754d11fa6b
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-17 15:01:10 +0300

    Create associations for the reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,11 @@
 class Reservation < ApplicationRecord
+  belongs_to :user,
+  belongs_to :car,
+
+  def confirm_reservation
+    self.confirmed = true
+    Reservation.create!(reservation_id: user_id,
+                       user_id: reservation_id,
+                       confirmed: true)
+  end
 end
