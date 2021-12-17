85e248256887a07a771b583550de7f7c65889676
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-17 13:24:53 +0300

    Create associations between car model and the corresponding ones

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,11 @@
 class Car < ApplicationRecord
+  belongs_to :user,
+  belongs_to :reservation
+
+  validates :model, presence: true, length: { maximum: 20,
+                                                too_long: '20 characters is the maximum allowed.' }
+
+  validates :image, presence: true,
+  validates :mileage, presence: true
+  validates :price, presence: true
 end
