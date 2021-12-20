1f96d4f887cb069874d262ed5a6a834fc2f2d338
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-20 18:06:30 +0300

    Validate the data to be held on the new columns

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+  
+  validates :agreement, presence: true length: { maximum: "100" }
+  validates :city, presence: true length: { maximum: "20" }
+  validates :date, presence: true
+
 
   def confirm_reservation
     self.confirmed = true
