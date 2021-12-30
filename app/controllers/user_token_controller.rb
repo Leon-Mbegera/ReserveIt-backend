8b4e374d30fcfa162d5f15a453cb69a23a7ac25c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-12-30 13:49:15 +0300

    Include a check to get username alongside their token

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -2,6 +2,6 @@ class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
   def create
     user_token = auth_token.token
-    render json: { token: user_token }
+    render json: { username: User.find(auth_token.payload[:sub]).username, token: user_token }, status: :created
   end
 end
