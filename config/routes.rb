6373d021a59048117bdafc64e95dc19c4e532fd5
Leon-Mbegera <mbegeraleon89@gmail.com>
2022-01-25 17:32:22 +0300

    Create a route to authenticate user token

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   scope '/auth' do
     post '/signin', to: 'user_token#create'
     post '/signup', to: 'users#create'
+    post '/authenticate' to: 'users#signed_in'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
